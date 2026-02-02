3d33d68d4d4d46eaa43f77d21d9d485cbbf84d85
badree <ayumguring093@gmail.com>
2026-02-02 16:20:06 +0700

    Feat : demo 1.6

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,4 +3,9 @@ import { defineConfig } from 'prisma/config'
 
 export default defineConfig({
     schema: path.join(__dirname, 'prisma', 'schema.prisma'),
+    datasource: {
+        provider: 'postgresql',
+        url: process.env.DATABASE_URL!,
+        directUrl: process.env.DIRECT_URL!,
+    },
 })
